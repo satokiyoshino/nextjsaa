e3a94225eedcf71867aa45f9ec17b5ae5f8491ec
satokiyoshino <orangerange0819sy@gmail.com>
2021-02-28 21:29:26 +0900

    add tournaments show

--- app/src/pages/rooms/[roomId]/tournaments/[tournamentId].tsx
+++ app/src/pages/rooms/[roomId]/tournaments/[tournamentId].tsx
@@ -2,6 +2,16 @@ import { useState, useEffect } from "react";
 
 const TournamentShow = (props) => {
     const {tournamentId} = props
+
+    const fetchTournamentPlayer = async(tournamentId) => {
+        const res = await fetch(`http://localhost/api/v1/tournaments/${tournamentId}`)
+        return await res.json()
+    }
+
+    useEffect(()=>{
+        fetchTournamentPlayer(tournamentId).then(data=>console.log(data))
+    },[])
+
     return(
         <>
             <h1>TournamentShow: {tournamentId}</h1>
